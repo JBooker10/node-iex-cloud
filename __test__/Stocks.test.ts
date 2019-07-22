903aa992c9fab204dc9b5d316d73e759c5bd4837
Jared Booker <jbooker@digitalmint.io>
2019-07-21 22:06:05 -0500

    updated

---
+++ __test__/Stocks.test.ts
@@ -0,0 +1,66 @@
+import IEXCloudClient from "../index";
+const fetch = require("node-fetch");
+const config = require("./example");
+
+const iex = new IEXCloudClient(fetch, config);
+
+test("Company Data", () => {
+  return iex
+    .symbol("AAPL")
+    .company()
+    .then(res => expect(res).toHaveProperty("symbol"));
+});
+
+test("Book Data", () => {
+  return iex
+    .symbol("AAPL")
+    .book()
+    .then(res => expect(res).toHaveProperty("quote"));
+});
+
+test("Balance Sheet Data", () => {
+  return iex
+    .symbol("AAPL")
+    .balanceSheet()
+    .then(res => expect(res).toHaveProperty("balancesheet"));
+});
+
+test("Chart data", () => {
+  return iex
+    .symbol("AAPL")
+    .chart()
+    .then(res => expect(res[0]).toHaveProperty("open"));
+});
+
+test("Deep Data", () => {
+  return iex
+    .symbol("AAPL")
+    .deep("book")
+    .then(res => expect(res).toMatchObject({}));
+});
+
+test("Ceo Compensation Data", () => {
+  return iex
+    .symbol("AAPL")
+    .ceoCompensation()
+    .then(res => expect(res).toHaveProperty("symbol"));
+});
+
+test("Batch Request", () => {
+  return iex
+    .symbol("googl")
+    .batch("company", "balance-sheet", "cash-flow", "estimates")
+    .then(res => expect(res).toHaveProperty("company.symbol"));
+});
+
+test("Today Earnings Data", () => {
+  return iex
+    .market("today-earnings")
+    .then(res => expect(res).toHaveProperty("amc"));
+});
+
+test("Upcoming Earnings Data", () => {
+  return iex
+    .market("upcoming-earnings")
+    .then(res => expect(res[0]).toHaveProperty("symbol"));
+});
